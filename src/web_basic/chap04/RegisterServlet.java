4079e27b3cc35f41ac61b1fe7937e875a6756dae
yjkim <chitose_@naver.com>
2021-03-23 19:23:06 +0900

    회원가입화면 마저하기

--- src/web_basic/chap04/RegisterServlet.java
+++ src/web_basic/chap04/RegisterServlet.java
@@ -10,13 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/chap04/registerServlet")
+//@WebServlet("/chap04/Register")
+@WebServlet("/chap04/Register")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		String id = request.getParameter("user-id");  //id에 있는 내용을 가져온다
+		String password = request.getParameter("pwd1");  //비밀번호도 가져오겠당
+		System.out.println("id : " + id + "password : " + password);
+		
+		
+		//한글 깨짐 처리
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out =  response.getWriter();
+		out.printf("아이디 = %s <br> 비밀번호 = %s",id,password);
 		process(request, response);
 	}
 
@@ -32,46 +42,58 @@ public class RegisterServlet extends HttpServlet {
 
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
+		
+		
 
-		String subject = request.getParameter("subject"); // 과목에 있는 내용 가져온다
-		String[] gubuns = request.getParameterValues("gubun"); // 수강구분에 있는 내용 가져온다
-		System.out.println("subject : " + subject + "gubuns : " + gubuns);
+		String subject = request.getParameter("subject"); // 선택한 과목(하나만) 가져오기
+		String[] gubuns = request.getParameterValues("gubun"); // 선택한 수강분야(여러개)
+		/*
+		 * for(int i = 0;i<gubun.length;i++) { System.out.println("<td>");
+		 * System.out.println(gubuns[i]); System.out.println("</td>"); }
+		 */
 		PrintWriter out = response.getWriter();
 
-		out.println("<html>");
-		out.printf("아이디 %s<br>비밀번호%s<br>", id, pw);
-		out.println("<h2>수강과목</h2>");
+	//	out.printf("아이디 %s<br>비밀번호%s<br>", id, pw);
+		out.println("<h2>수강과목 : </h2>");
 		String t = null;
-		if (subject.equals("eng")) {
+		if(subject==null) {
+			out.println("선택한 수강과목이 없음");
+			
+		}else if (subject.equals("eng")) {
 			t = "영어";
+			out.println(t);
 		} else if (subject.equals("ch")) {
 			t = "중국어회화";
+			out.println(t);
 		} else {
 			t = "일어회화";
-
+			out.println(t);
 		}
 
-		out.println("수강분야");
+		out.println("<h2>수강분야 : </h2>");
+		String g = null;
 		if (gubuns == null) {
 			out.println("선택한 수강분야가 없음");
 
 		} else {
 			for (String gubun : gubuns) {
 				if (gubun.equals("grm")) {
-
+					g = "문법";
+					out.println(g);
 				}
 				if (gubun.equals("wr")) {
-
+					g = "작문";
+					out.println(g);
 				}
-				if (gubun.equals("grm")) {
-
+				if (gubun.equals("rd")) {
+					g= "독해";
+					out.println(g);
 				}
 			}
 		}
 
-		out.printf("수강과목 = %s <br> 수강구분 = %s", subject, gubuns);
 
-		System.out.println("subject : " + subject + "gubuns : " + gubuns);
+	
 
 	}
 }
